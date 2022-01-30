f6c5114064f773aa50155478bbb2e19fd0e299ac
David Diaz <daviddiazlds1221@gmail.com>
2022-01-29 23:13:21 -0500

    Se obtiene el id map desde el js y se crea evento de escucha obteniendo las distancias en X y Y

--- js/game.js
+++ js/game.js
@@ -1,3 +1,14 @@
 const WIDTH = 400;
 const HEIGH = 400;
 
+let target = {
+    x: getRandomNumber(WIDTH),
+    y: getRandomNumber(HEIGH)
+};
+
+let $map = document.getElementById("map");
+
+$map.addEventListener("click", function(e){
+    let distance = getDistance(e, target);
+    let distanceHint = getDistanceHint(distance);
+});

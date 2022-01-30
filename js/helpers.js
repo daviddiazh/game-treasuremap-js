9da7d6581918002aa6d84022810ad5ca4a42f12b
David Diaz <daviddiazlds1221@gmail.com>
2022-01-29 22:27:02 -0500

    Se crea funcion de obtener distancia del tesoro

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,9 @@
 const getRandom = size => {
     return Math.floor(Math.random() * size);
 }
+
+const getDistance = (e, target) => {
+    let diffX = e.offsetX - target.x;
+    let diffY = e.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY + diffY));
+}
